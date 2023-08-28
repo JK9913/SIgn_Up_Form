b3008f62cfd2c2c890e75397747a0a32266f98a6
Jan-Kristian Karlsen <janke3567@gmail.com>
2023-08-28 22:26:00 +0200

    Created validation for email input as a test, and will implement this on every input in the form.

--- script.js
+++ script.js
@@ -23,4 +23,20 @@ form.addEventListener("submit", (event) => {
         // Cancel the submition as the email field is not valid
         event.preventDefault();
     }
-})
+});
+
+function showError() {
+    if (email.validity.valueMissing) {
+        // For when the field is empty
+        emailError.textContent = "You need to enter an email address.";
+    } else if (email.validity.typeMismatch) {
+        // For when doesn't contain the correct type of value
+        emailError.textcontent = "Entered value needs to be an email address.";
+    } else if (email.validity.tooShort) {
+        // For when the data is too short
+        emailError.textContent = `Email should be at least ${email.minLength} characters.`;
+    }
+
+    // Set styling
+    emailError.className = "error active"
+}
