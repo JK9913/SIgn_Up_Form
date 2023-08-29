daa5085be6f8c4e75ff832ed492760ad83c7fa8b
Jan-Kristian Karlsen <janke3567@gmail.com>
2023-08-29 22:23:35 +0200

    Created a better javascript ehich takes all inputs into one variable and uses forEach to iterate through everyone and validate if they are correct. Still needs to add an own message to each one

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
 const form = document.querySelector("form")
-const email = document.getElementById("mail");
-const emailError = document.querySelector("#mail + span.error");
+//const email = document.getElementById("mail");
+//const emailError = document.querySelector("#mail + span.error");
 
-email.addEventListener("input", (event) => {
+// test
+const fields = document.querySelectorAll("input");
+const errors = document.querySelectorAll("span");
+// test
+
+fields.forEach((field) => {
+    field.addEventListener("input", (event) => {
+        // Chooses the next sibling in the HTML structure, which should be a SPAN
+        var spanError = field.nextElementSibling;
+
+        if (field.validity.valid) {
+
+            spanError.textContent = "";
+            spanError.className = "error";
+    
+        } else {
+            showError(field, spanError);
+        } 
+    });
+    
+});
+
+
+
+/*email.addEventListener("input", (event) => {
 
     if (email.validity.valid) {
 
@@ -13,19 +37,59 @@ email.addEventListener("input", (event) => {
         showError();
     } 
     
-});
+});*/
 
 form.addEventListener("submit", (event) => {
+    console.log("This works...");
+    fields.forEach((field) => {
+        var spanError = field.nextElementSibling;
+        console.log(`${field.getAttribute}`);
+        if (!field.validity.valid) {
+            
+            showError(field, spanError);
+    
+            // Cancel the submition as the email field is not valid
+            event.preventDefault();
+        }
+    });
 
-    if (!email.validity.valid) {
+
+    /*if (!email.validity.valid) {
         showError();
 
         // Cancel the submition as the email field is not valid
         event.preventDefault();
-    }
+    }*/
 });
+//
+//
+//
+//Test
+function showError(input, errorField) {
+    if (input.validity.valueMissing) {
+        // For when the field is empty
+        console.log("The field is empty");
+        errorField.textContent = "You need to enter an email address.";
+    } else if (input.validity.typeMismatch) {
+        console.log("Wrong type")
+        // For when doesn't contain the correct type of value
+        errorField.textContent = "Entered value needs to be an email address.";
+    } else if (input.validity.tooShort) {
+        console.log("Too short")
+        // For when the data is too short
+        errorField.textContent = `Email should be at least ${input.minLength} characters.`;
+    }
 
-function showError() {
+    // Set styling
+    errorField.className = "error active"
+};
+//Test
+//
+//
+//
+
+
+/*function showError() {
     if (email.validity.valueMissing) {
         // For when the field is empty
         emailError.textContent = "You need to enter an email address.";
@@ -39,4 +103,4 @@ function showError() {
 
     // Set styling
     emailError.className = "error active"
-}
+}*/
